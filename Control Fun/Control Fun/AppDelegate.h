3d23f7fead97a76e5c37f70bdff4dc513a189be8
qizl <q@qizl.cn>
2016-06-23 21:04:22 +0800

    -

---
+++ Control Fun/Control Fun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Control Fun
+//
+//  Created by qizl on 16/6/23.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Control Fun/Control Fun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Control Fun
+//
+//  Created by qizl on 16/6/23.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
