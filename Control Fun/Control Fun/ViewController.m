4917d3febd18998c1bc28ef8756e6d9626226f05
qizl <q@qizl.cn>
2016-06-25 01:58:02 +0800

    -

--- Control Fun/Control Fun/ViewController.m
+++ Control Fun/Control Fun/ViewController.m
@@ -13,6 +13,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *numberField;
 @property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
 @property (weak, nonatomic) IBOutlet UISlider *pgr;
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *doSomethingButton;
 
 @end
 
@@ -43,6 +46,27 @@
 - (IBAction)sliderChanged:(UISlider *)sender {
     int progress = (int)lround(sender.value);
     self.sliderLabel.text = [NSString stringWithFormat:@"%d",progress];
+}
+
+- (IBAction)switchChanged:(UISwitch *)sender {
+    BOOL setting = sender.isOn;
+    [self.leftSwitch setOn:setting animated:YES];
+    [self.rightSwitch setOn:setting animated:YES];
+}
+- (IBAction)toggleControls:(UISegmentedControl *)sender {
+    if(sender.selectedSegmentIndex == 0)
+    {
+        self.leftSwitch.hidden = NO;
+        self.rightSwitch.hidden = NO;
+        self.doSomethingButton.hidden = YES;
+    } else {
+        self.leftSwitch.hidden = YES;
+        self.rightSwitch.hidden = YES;
+        self.doSomethingButton.hidden = NO;
+    }
+}
+
+- (IBAction)buttonPresses:(UIButton *)sender {
 }
 
 @end
