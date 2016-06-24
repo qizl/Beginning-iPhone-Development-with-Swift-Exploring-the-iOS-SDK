c70ae6d91cd2c4eec7d93fe274b54d0fcf076174
qizl <q@qizl.cn>
2016-06-25 00:36:41 +0800

    -

--- Control Fun/Control Fun/ViewController.m
+++ Control Fun/Control Fun/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *numberField;
 
 @end
 
@@ -22,6 +24,17 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)textFieldDoneEditing:(id)sender
+{
+    [sender resignFirstResponder];
+}
+
+-(IBAction)backgroundTap:(id)sender
+{
+    [self.nameField resignFirstResponder];
+    [self.numberField resignFirstResponder];
 }
 
 @end
