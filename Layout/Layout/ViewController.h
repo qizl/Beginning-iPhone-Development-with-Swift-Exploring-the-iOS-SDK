d4cb8fc892ef967c45f697bb3ab644b3dd706013
qizl <q@qizl.cn>
2016-06-25 17:03:54 +0800

    -

---
+++ Layout/Layout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Layout
+//
+//  Created by qizl on 16/6/25.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Layout/Layout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Layout
+//
+//  Created by qizl on 16/6/25.
+//  Copyright © 2016年 qizl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
